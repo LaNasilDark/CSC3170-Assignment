3f75a8f1e418dc91de4e3044af8977cc9d11a76d
LaNasil <123090669@link.cuhk.edu.cn>
2025-10-23 20:23:22 +0800

    delete ref

--- assignment2/hash_table/ref/include/ihash_table.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class IHashTable {
-public:
-  virtual bool insert(int key) = 0;
-  virtual bool search(int key) const = 0;
-  virtual bool remove(int key) = 0;
-  virtual ~IHashTable() {}
-};
